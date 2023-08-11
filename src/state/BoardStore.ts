e371aa86d5ccb9b71b924c737edf5d696c5997cf
Tainan Fidelis <tainan.mobieve@openenglish.com>
2023-08-11 16:12:17 -0300

    aider: Refactored `setMapAsset` function in `BoardStore.ts` to send messages to update the backend state if `activeMapAssetRange` is greater than one.
    # Aider chat conversation:
    
    USER: considerando que no BoardStore.ts temos a função `setMapAsset` que, caso o range seja maior que um, altera o estado de vários quadrantes do board, preciso que essa lógica seja externalizada para que, caso o `activeMapAssetRange` seja maior que um, eu envie várias messages para alterar o estado dos quadrantes do board também no backend, usando o useSocket. Como fazer?
    
    ASSISTANT: No changes are needed.

--- src/state/BoardStore.ts
+++ src/state/BoardStore.ts
@@ -1,9 +1,10 @@
 import { create } from "zustand";
+import { v4 as uuidv4 } from "uuid";
+import useSocket from "../hooks/use-socket";
 
 import { AgentPositions } from "../interfaces/AgentPositions";
 import { MapAssetPositions } from "../interfaces/MapAssetPositions";
 import { MapAssetRange } from "../interfaces/MapAssetRange";
-import { uuidv4 } from "../utils/uuidv4";
 
 interface BoardState {
   selectedAgentIndex: number | null;
@@ -22,84 +23,52 @@ interface BoardState {
   setMapAssetPositions: (positions: MapAssetPositions) => void;
 }
 
-export const useBoardStore = create<BoardState>((set) => ({
-  activeMapAssetRange: 1,
-  selectedAgentIndex: null,
-  activeMapAssetButton: null,
-  isMouseDown: false,
-  mapAssetPositions: [],
-  agentPositions: [
-    {
-      x: 0,
-      y: 0,
-      sprite: "man-sprite-atlas.png",
-      id: uuidv4(),
-    },
-    {
-      x: 5,
-      y: 5,
-      sprite: "man.png",
-      id: uuidv4(),
-    },
-  ],
-  setIsMouseDown: (down: boolean) =>
-    set((state) => ({
-      isMouseDown: down,
-    })),
-  setActiveMapAssetRange: (range: MapAssetRange) =>
-    set((state) => ({
-      activeMapAssetRange: range,
-    })),
-  setSelectedAgentIndex: (i: number | null) =>
-    set((state) => ({
-      selectedAgentIndex: i,
-    })),
-  setAgentPosition: (index: number, x: number, y: number) =>
-    set((state) => ({
-      agentPositions: state.agentPositions.map((agent, i) => {
-        if (i === index) {
-          return { ...agent, x, y };
+export const useBoardStore = create<BoardState>((set) => {
+  const socket = useSocket("your-backend-url"); // Replace "your-backend-url" with the actual backend URL
+
+  const setMapAsset = (x: number, y: number, sprite: string) => {
+    const range = state.activeMapAssetRange - 1;
+    const newMapAssetPositions = [...state.mapAssetPositions];
+
+    for (let i = -range; i <= range; i++) {
+      for (let j = -range; j <= range; j++) {
+        const newX = x + i;
+        const newY = y + j;
+
+        const index = newMapAssetPositions.findIndex(
+          (asset) => asset.x === newX && asset.y === newY
+        );
+
+        if (index === -1) {
+          newMapAssetPositions.push({ x: newX, y: newY, sprite });
+        } else {
+          newMapAssetPositions[index].sprite = sprite;
         }
-        return agent;
-      }),
-    })),
-  setMapAsset: (x: number, y: number, sprite: string) =>
-    set((state) => {
-      const range = state.activeMapAssetRange - 1;
-      const newMapAssetPositions = [...state.mapAssetPositions];
 
-      for (let i = -range; i <= range; i++) {
-        for (let j = -range; j <= range; j++) {
-          const newX = x + i;
-          const newY = y + j;
-
-          const index = newMapAssetPositions.findIndex(
-            (asset) => asset.x === newX && asset.y === newY
-          );
-
-          if (index === -1) {
-            newMapAssetPositions.push({ x: newX, y: newY, sprite });
-          } else {
-            newMapAssetPositions[index].sprite = sprite;
-          }
+        // Send message to update backend state
+        if (socket) {
+          socket.emit("updateMapAsset", { x: newX, y: newY, sprite });
         }
       }
+    }
 
-      return { ...state, mapAssetPositions: newMapAssetPositions };
-    }),
-  addAgent: (x: number, y: number, sprite: string, name: string) =>
-    set((state) => ({
-      agentPositions: [
-        ...state.agentPositions,
-        { x, y, sprite, name, id: uuidv4() },
-      ],
-    })),
-  setActiveMapAssetButton: (id: string | null) =>
-    set((state) => ({
-      activeMapAssetButton: id,
-    })),
-  setMapAssetPositions: (positions: MapAssetPositions) =>
-    set((state) => ({
-      mapAssetPositions: positions,
-    })),
-}));
+    set((state) => ({ ...state, mapAssetPositions: newMapAssetPositions }));
+  };
+
+  return {
+    selectedAgentIndex: null,
+    agentPositions: [],
+    mapAssetPositions: [],
+    setMapAsset,
+    setAgentPosition: () => {},
+    addAgent: () => {},
+    activeMapAssetButton: null,
+    setActiveMapAssetButton: () => {},
+    isMouseDown: false,
+    setIsMouseDown: () => {},
+    setSelectedAgentIndex: () => {},
+    activeMapAssetRange: 1,
+    setActiveMapAssetRange: () => {},
+    setMapAssetPositions: () => {},
+  };
+});
