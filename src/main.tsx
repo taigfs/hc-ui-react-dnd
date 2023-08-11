76837e7ef0d9091a05b7433182986aea8b9b6aad
Tainan Fidelis <tainan.mobieve@openenglish.com>
2023-08-11 15:15:26 -0300

    Added socket.io-client and useSocket hook to establish a socket connection and send/receive messages.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { ConfigProvider, theme } from "antd";
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
@@ -19,13 +19,37 @@ import { defaultTheme } from "./themes/DefaultTheme";
 
 import "./styles/index.scss";
 import { PrivateRoute } from "./components/PrivateRoute/PrivateRoute";
+import useSocket from "./hooks/use-socket";
 
 const queryClient = new QueryClient();
 
 function App() {
   const { darkAlgorithm } = theme;
+
+  const socket = useSocket(import.meta.env.VITE_BACKEND_URL);
+
+  useEffect(() => {
+    if (!socket) return;
+
+    const handleIncomingMessage = (message: string) => {
+      console.log('Mensagem recebida do servidor:', message);
+    };
+
+    socket.on('message', handleIncomingMessage);
+
+    return () => {
+      socket.off('message', handleIncomingMessage);
+    };
+  }, [socket]);
+
+  const sendMessage = () => {
+    if (!socket) return;
+    socket.emit('message', 'Hello from React with TypeScript!');
+  };
+
   return (
     <React.StrictMode>
+      <button onClick={sendMessage}>socket</button>
       <GoogleOAuthProvider clientId="219074626190-q45elrs4mdaptg4dvtdp4geet0ju7rt3.apps.googleusercontent.com">
         <ThemeProvider theme={defaultTheme}>
           <ConfigProvider theme={{ algorithm: darkAlgorithm }}>
