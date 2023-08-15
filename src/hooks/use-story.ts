8eb2018907574d76b4530bdda8e6fc0083fdec9d
Tainan Fidelis <tainan.mobieve@openenglish.com>
2023-08-15 15:27:17 -0300

    Add useGetAgentSprites hook to fetch agent sprites.

--- src/hooks/use-story.ts
+++ src/hooks/use-story.ts
@@ -2,6 +2,7 @@ import { useQuery, useMutation, useQueryClient } from 'react-query';
 import axios from 'axios';
 import { StoryService } from '../services/story.service';
 import { StoryDto } from '../types/StoryDto';
+import { AgentSpriteService } from '../services/agent-sprite.service';
 
 export function useGetStories(projectId: number) {
   return useQuery('stories', async () => StoryService.getStories(projectId));
@@ -14,4 +15,8 @@ export function usePostStory() {
       queryClient.invalidateQueries('stories');
     }
   });
+}
+
+export function useGetAgentSprites() {
+  return useQuery('agentSprites', async () => AgentSpriteService.getAgentSprites());
 }
