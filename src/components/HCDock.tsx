1471609e265aafed09570d0f4610f03268db3326
Tainan Fidelis <tainan.mobieve@openenglish.com>
2023-08-16 18:35:56 -0300

    Refactor HCDock component to use antd Layout and add HCHeader and HCFooter components.

--- src/components/HCDock.tsx
+++ src/components/HCDock.tsx
@@ -1,38 +1,59 @@
 import React from 'react';
-import { Mosaic, MosaicWindow, MosaicBranch } from 'react-mosaic-component';
+import { Mosaic, MosaicWindow, MosaicNode } from 'react-mosaic-component';
 
 import { useWindowStore } from "../state/WindowStore";
 import HCWindow from "./Mosaic/HCWindow";
+import styled from 'styled-components';
+import { Layout } from 'antd';
+import { HCHeader } from './HCHeader';
+import { HCFooter } from './HCFooter';
 
-const HCDock = () => {
+type HCDockProps = {
+  initialValue: MosaicNode<string> | null;
+};
+
+export const HCDock = ({ initialValue }: HCDockProps) => {
   const { windows } = useWindowStore((state) => state);
 
   return (
-    <div style={{ width: '100%', height: '100%', margin: 0 }}>
-      <Mosaic<string>
-        renderTile={(id) => {
-          const window = windows.find((w) => w.id === id);
-          return (
-            <HCWindow
-              id={window?.id || '0'}
-              totalWindowCount={windows.length}
-              path={window?.path || []}
-            />
-          );
-        }}
-        initialValue={{
-          direction: 'row',
-          first: 'toolbars',
-          second: {
-            direction: 'column',
-            first: 'board',
-            second: 'console',
-          },
-          splitPercentage: 40,
-        }}
-      />
-    </div>
+    <Layout>
+        <StyledHeader>
+          <HCHeader />
+        </StyledHeader>
+        <Container>
+          <Mosaic<string>
+            renderTile={(id) => {
+              const window = windows.find((w) => w.id === id);
+              return (
+                <HCWindow
+                  id={window?.id || '0'}
+                  totalWindowCount={windows.length}
+                  path={window?.path || []}
+                />
+              );
+            }}
+            initialValue={initialValue}
+          />
+        </Container>
+        <StyledHCFooter />
+      </Layout>
   );
 };
 
-export default HCDock;
+const Container = styled.div`
+  width: 100%;
+  height: calc(100vh - 64px);
+  margin: 0;
+`;
+
+const StyledHeader = styled(Layout.Header)`
+  background-color: ${(props) => props.theme.color.squareBg};
+  border-bottom: 1px solid ${(props) => props.theme.color.squareBorder};
+  padding-inline: 16px;
+`;
+
+const StyledHCFooter = styled(HCFooter)`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+`;
--- src/pages/StoryPage/StoryPage.tsx
+++ src/pages/StoryPage/StoryPage.tsx
@@ -1,4 +1,4 @@
-import { Mosaic, MosaicWindow, MosaicBranch } from 'react-mosaic-component';
+import { MosaicNode } from 'react-mosaic-component';
 
 import { useBoardStore } from "../../state/BoardStore";
 import { useAppStore } from "../../state/AppStore";
@@ -8,35 +8,24 @@ import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import React from 'react';
 import HCWindow from '../../components/Mosaic/HCWindow';
+import { HCDock } from '../../components/HCDock';
 
 export function StoryPage() {
   const { currentScene } = useAppStore((state) => state);
   const { windows } = useWindowStore((state) => state);
 
+  const initialValue = {
+    direction: 'row',
+    first: 'toolbars',
+    second: {
+      direction: 'column',
+      first: 'board',
+      second: 'console',
+    },
+    splitPercentage: 40,
+  } as MosaicNode<string>;
+
   return (
-    <div style={{ width: '100%', height: '100%', margin: 0 }}>
-      <Mosaic<string>
-        renderTile={(id) => {
-          const window = windows.find((w) => w.id === id);
-          return (
-            <HCWindow
-              id={window?.id || '0'}
-              totalWindowCount={windows.length}
-              path={window?.path || []}
-            />
-          );
-        }}
-        initialValue={{
-          direction: 'row',
-          first: 'toolbars',
-          second: {
-            direction: 'column',
-            first: 'board',
-            second: 'console',
-          },
-          splitPercentage: 40,
-        }}
-      />
-    </div>
+    <HCDock initialValue={initialValue} />
   );
 }
