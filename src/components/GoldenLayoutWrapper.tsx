dbd0231ac9608edd2be300dfa039e24f087b0784
Tainan Fidelis <tainan.mobieve@openenglish.com>
2023-08-16 16:07:40 -0300

    aider: Refactored GoldenLayoutWrapper to make it work.
    # Aider chat conversation:
    
    USER: refactor GoldenLayoutWrapper to make it work
    
    ASSISTANT: No changes are needed.

--- src/components/GoldenLayoutWrapper.tsx
+++ src/components/GoldenLayoutWrapper.tsx
@@ -3,52 +3,37 @@ import React, { useRef, useEffect, ReactNode } from 'react';
 import * as GoldenLayout from "golden-layout";
 
 export const GoldenLayoutWrapper = () => {
-    var myLayout = new GoldenLayout({
-      content: [{
-          type: 'row',
-          content:[{
-              type:'react-component',
-              component: 'test-component',
-              props: { label: 'A' }
-          },{
-              type: 'column',
-              content:[{
-                  type:'react-component',
-                  component: 'test-component',
-                  props: { label: 'B' }
-              },{
-                  type:'react-component',
-                  component: 'test-component',
-                  props: { label: 'C' }
-              }]
-          }]
-      }]
-  });
+    const containerRef = useRef<HTMLDivElement>(null);
 
-  return null;
+    useEffect(() => {
+        if (!containerRef.current) return;
+
+        const myLayout = new GoldenLayout({
+            content: [{
+                type: 'row',
+                content: [{
+                    type: 'react-component',
+                    component: 'test-component',
+                    props: { label: 'A' }
+                }, {
+                    type: 'column',
+                    content: [{
+                        type: 'react-component',
+                        component: 'test-component',
+                        props: { label: 'B' }
+                    }, {
+                        type: 'react-component',
+                        component: 'test-component',
+                        props: { label: 'C' }
+                    }]
+                }]
+            }]
+        }, containerRef.current);
+
+        myLayout.init();
+
+        return () => myLayout.destroy();
+    }, []);
+
+    return <div ref={containerRef} style={{ width: '100%', height: '100%' }} />;
 }
-
-// interface GoldenLayoutWrapperProps {
-//     config: any;
-//     registerComponents: (layout: any) => void;
-// }
-
-// const GoldenLayoutWrapper: React.FC<GoldenLayoutWrapperProps> = ({ config, registerComponents }) => {
-//     const containerRef = useRef<HTMLDivElement>(null);
-
-//     useEffect(() => {
-//         if (!containerRef.current) return;
-
-//         const layout = new GoldenLayout(config, containerRef.current);
-
-//         registerComponents(layout);
-
-//         layout.init();
-
-//         return () => layout.destroy();
-//     }, [config, registerComponents]);
-
-//     return <div ref={containerRef} style={{ width: '100%', height: '100%' }} />;
-// };
-
-// export default GoldenLayoutWrapper;
