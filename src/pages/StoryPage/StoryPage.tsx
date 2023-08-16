730b4e0205e044848e2d3f9ceb6654b8f49cf0fd
Tainan Fidelis <tainan.mobieve@openenglish.com>
2023-08-16 18:52:11 -0300

    Set initial value for mosaicNodes in WindowStore.

--- src/pages/StoryPage/StoryPage.tsx
+++ src/pages/StoryPage/StoryPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { MosaicNode } from 'react-mosaic-component';
 
 import { useAppStore } from "../../state/AppStore";
@@ -6,20 +6,25 @@ import 'react-mosaic-component/react-mosaic-component.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import { HCDock } from '../../components/HCDock';
+import { useWindowStore } from '../../state/WindowStore';
 
 export function StoryPage() {
   const { currentScene } = useAppStore((state) => state);
+  const { mosaicNodes, setMosaicNodes } = useWindowStore((state) => state);
 
-  const initialValue = {
-    direction: 'row',
-    first: 'toolbars',
-    second: {
-      direction: 'column',
-      first: 'board',
-      second: 'console',
-    },
-    splitPercentage: 40,
-  } as MosaicNode<string>;
+  useEffect(() => {
+    const initialValue = {
+      direction: 'row',
+      first: 'toolbars',
+      second: {
+        direction: 'column',
+        first: 'board',
+        second: 'console',
+      },
+      splitPercentage: 40,
+    } as MosaicNode<string>;
+    setMosaicNodes(initialValue);
+  }, []);
 
   const ToolbarsComponent = () => <div>Toolbars</div>;
   const BoardComponent = () => <div>Board</div>;
@@ -32,6 +37,6 @@ export function StoryPage() {
   };
 
   return (
-    <HCDock initialValue={initialValue} components={components} />
+    <HCDock initialValue={mosaicNodes} components={components} />
   );
 }
--- src/state/WindowStore.ts
+++ src/state/WindowStore.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { MosaicBranch } from 'react-mosaic-component';
+import { MosaicBranch, MosaicNode } from 'react-mosaic-component';
 
 interface Window {
   id: string;
@@ -7,28 +7,15 @@ interface Window {
 }
 
 interface WindowState {
-  windows: Window[];
-  openWindow: (window: Window) => void;
-  closeWindow: (id: string) => void;
-  setWindows: (windows: Window[]) => void;
+  mosaicNodes: MosaicNode<string> | null;
+  setMosaicNodes: (mosaicNode: MosaicNode<string>) => void;
+  // openWindow: (window: Window) => void;
+  // closeWindow: (id: string) => void;
 }
 
 export const useWindowStore = create<WindowState>((set) => ({
-  windows: [
-    {
-      id: 'toolbars',
-      path: ['first'],
-    },
-    {
-      id: 'board',
-      path: ['second', 'first'],
-    },
-    {
-      id: 'console',
-      path: ['second', 'second'],
-    },
-  ],
-  openWindow: (window) => set((state) => ({ windows: [...state.windows, window] })),
-  closeWindow: (id) => set((state) => ({ windows: state.windows.filter((window) => window.id !== id) })),
-  setWindows: (windows) => set(() => ({ windows })),
+  mosaicNodes: null,
+  setMosaicNodes: (mosaicNodes) => set(() => ({ mosaicNodes })),
+  // openWindow: (window) => set((state) => ({ windows: [...state.windows, window] })),
+  // closeWindow: (id) => set((state) => ({ windows: state.windows.filter((window) => window.id !== id) })),
 }));
