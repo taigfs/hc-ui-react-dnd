d40d11c18e331f4e703946d51b5c0fcaf4460a1b
Tainan Fidelis <tainan.mobieve@openenglish.com>
2023-08-17 10:19:27 -0300

    aider: Create `SceneToolbarComponent` component inside `src/components/SceneToolbar.tsx` file.
    # Aider chat conversation:
    
    USER: create a component named `SceneToolbarComponent` inside `src/components/SceneToolbar.tsx` and include this code into it `const ToolbarsComponent = () => (
      <>
        <AgentsToolbar />
        <MapAssetsToolbar />
      </>
    );`
    
    ASSISTANT: No changes are needed.

---
+++ src/components/SceneToolbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AgentsToolbar from './AgentsToolbar';
+import MapAssetsToolbar from './MapAssetsToolbar';
+
+const SceneToolbarComponent = () => (
+  <>
+    <AgentsToolbar />
+    <MapAssetsToolbar />
+  </>
+);
+
+export default SceneToolbarComponent;
