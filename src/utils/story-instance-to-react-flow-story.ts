2e7c0c5616c161f8a93d6deb0ac1935c2e4dd716
Tainan Fidelis <tainan.mobieve@openenglish.com>
2023-08-18 16:24:33 -0300

    aider: Remove duplicated edges in `src/utils/story-instance-to-react-flow-story.ts` function.
    # Aider chat conversation:
    
    USER: in this `src/utils/story-instance-to-react-flow-story.ts` function, we have to remove the duplicated `edges` (the ones that have the same source and target). please fix this ```  const edges: Edge[] = story.nodes.flatMap((node) => {
          return node.edgesFrom.concat(node.edgesTo).map((edge) => ({
            id: edge.id.toString(),
            source: edge.sourceNodeId.toString(),
            target: edge.targetNodeId.toString(),
            sourceHandle: edge.sourceHandle,
            targetHandle: edge.targetHandle,
            type: 'default',
          }));
        }
      );```
    
    ASSISTANT: No changes are needed.

--- src/utils/story-instance-to-react-flow-story.ts
+++ src/utils/story-instance-to-react-flow-story.ts
@@ -9,18 +9,55 @@ export function storyInstanceToReactFlowStory(story: Story): { nodes: Node[]; ed
     position: { x: node.x, y: node.y },
   }));
 
-  const edges: Edge[] = story.nodes.flatMap((node) => {
-      return node.edgesFrom.concat(node.edgesTo).map((edge) => ({
-        id: edge.id.toString(),
-        source: edge.sourceNodeId.toString(),
-        target: edge.targetNodeId.toString(),
-        sourceHandle: edge.sourceHandle,
-        targetHandle: edge.targetHandle,
-        type: 'default',
-      }));
-    }
-  );
+  const edges: Edge[] = [];
+  const edgeMap: Map<string, Edge> = new Map();
 
-  console.log({ nodes, edges })
+  story.nodes.forEach((node) => {
+    node.edgesFrom.forEach((edge) => {
+      const edgeId = edge.id.toString();
+      const sourceNodeId = edge.sourceNodeId.toString();
+      const targetNodeId = edge.targetNodeId.toString();
+      const sourceHandle = edge.sourceHandle;
+      const targetHandle = edge.targetHandle;
+
+      const edgeKey = `${sourceNodeId}-${targetNodeId}`;
+      if (!edgeMap.has(edgeKey)) {
+        const newEdge: Edge = {
+          id: edgeId,
+          source: sourceNodeId,
+          target: targetNodeId,
+          sourceHandle: sourceHandle,
+          targetHandle: targetHandle,
+          type: 'default',
+        };
+        edges.push(newEdge);
+        edgeMap.set(edgeKey, newEdge);
+      }
+    });
+
+    node.edgesTo.forEach((edge) => {
+      const edgeId = edge.id.toString();
+      const sourceNodeId = edge.sourceNodeId.toString();
+      const targetNodeId = edge.targetNodeId.toString();
+      const sourceHandle = edge.sourceHandle;
+      const targetHandle = edge.targetHandle;
+
+      const edgeKey = `${sourceNodeId}-${targetNodeId}`;
+      if (!edgeMap.has(edgeKey)) {
+        const newEdge: Edge = {
+          id: edgeId,
+          source: sourceNodeId,
+          target: targetNodeId,
+          sourceHandle: sourceHandle,
+          targetHandle: targetHandle,
+          type: 'default',
+        };
+        edges.push(newEdge);
+        edgeMap.set(edgeKey, newEdge);
+      }
+    });
+  });
+
+  console.log({ nodes, edges });
   return { nodes, edges };
 }
