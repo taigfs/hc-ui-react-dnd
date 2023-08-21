f64d642c6f28c3185e9f1034dfe9765d52adcd09
Tainan Fidelis <tainan.mobieve@openenglish.com>
2023-08-21 20:40:39 -0300

    Creating EditNodeWindow;

---
+++ src/components/EditNodeWindow.tsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useNodeAndEdgeInstance } from '../hooks/use-story';
+import { PatchNodeDTO } from '../dtos/patch-node-dto';
+import { ToolbarContainer } from './Toolbar/styles';
+import { useDiagramStore } from '../state/DiagramStore';
+import styled from 'styled-components';
+import { getValueAfterUnderscore } from '../utils/get-value-after-underscore';
+import { Button } from 'antd';
+
+export const EditNodeWindow: React.FC = () => {
+  const { register, handleSubmit, setValue } = useForm();
+  const { patchNode } = useNodeAndEdgeInstance();
+  const { selectedNode: node, setSelectedNode, updateNodeLabel } = useDiagramStore((s) => s);
+  
+  if (!node) { return null; }
+  console.log(node);
+
+  const id = getValueAfterUnderscore(node.id);
+
+  setValue('label', node.data?.label);
+
+  if (node.type === 'move') {
+    setValue('actionData.moveToX', node.data?.actionData?.moveToX);
+    setValue('actionData.moveToY', node.data?.actionData?.moveToY);
+  }
+
+  if (node.type === 'script') {
+    setValue('scriptUrl', node.data?.actionData?.scriptUrl);
+  }
+
+
+  const onSubmit = (data: any) => {
+    if (data.actionData?.moveToX && data.actionData?.moveToY) {
+      data.actionData.moveToX = Number(data.actionData?.moveToX);
+      data.actionData.moveToY = Number(data.actionData?.moveToY);
+    }
+
+    const dto: PatchNodeDTO = {
+      id,
+      updates: {
+        label: data.label,
+        x: node.position.x,
+        y: node.position.y,
+        data: {
+          ...node.data,
+          actionData: data.actionData,
+        }
+      }
+    };
+    console.log(dto);
+    patchNode(dto);
+    updateNodeLabel(node.id, data.label);
+    setSelectedNode(null);
+  };
+
+  return (
+    <StyledToolbarContainer>
+      <StyledH4>Edit Node #{id}</StyledH4>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {node.type === 'move' && (
+          <>
+            <StyledInput {...register('label')} placeholder="Label" />
+            <StyledInput {...register('actionData.moveToX')} type="number" placeholder="Move to X" />
+            <StyledInput {...register('actionData.moveToY')} type="number" placeholder="Move to Y" />
+          </>
+        )}
+
+        {node.type === 'script' && (
+          <>
+            <StyledInput {...register('label')} placeholder="Label" />
+            <StyledInput {...register('scriptUrl')} placeholder="Script URL" />
+          </>
+        )}
+
+        <Button type="primary" htmlType="submit">Update</Button>
+      </form>
+    </StyledToolbarContainer>
+  );
+};
+
+const StyledH4 = styled.h4`
+  position: sticky;
+  top: 0;
+  background-color: #151515;
+  z-index: 10;
+  padding: 8px 0 4px;
+`;
+
+const StyledInput = styled.input`
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 8px;
+`;
+
+const StyledToolbarContainer = styled(ToolbarContainer)`
+  padding-bottom: 8px;
+`;
