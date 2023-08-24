d77cb634f995cb4038dc9598e2769623434a2618
Tainan Fidelis <tainan.mobieve@openenglish.com>
2023-08-24 16:51:14 -0300

    aider: Improve FolderFile style using styled-components, add left padding to indicate it's inside a folder, add cursor pointer on hover, add different icon before text for each file type using antd icons, and add larger spacing between files.
    # Aider chat conversation:
    
    USER: agora quero que melhore o estilo de folderfile, usando styled-components, adicione o espaçamento esquerda para mostrar que está dentro da pasta. adicione cursor pointer on hover. adicione um ícone diferente antes do texto para cada tipo de file. use antd icons. adicione um espaçamento maior entre os arquivos.
    
    ASSISTANT: No changes are needed.

--- src/components/FolderFile.tsx
+++ src/components/FolderFile.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import styled from 'styled-components';
+import { FileOutlined, FileTextOutlined } from '@ant-design/icons';
 
 interface FolderFileProps {
   fileName: string;
@@ -7,11 +9,39 @@ interface FolderFileProps {
 }
 
 const FolderFile: React.FC<FolderFileProps> = ({ fileName, fileType, onClick }) => {
+  const getFileIcon = () => {
+    switch (fileType) {
+      case 'story':
+        return <FileTextOutlined />;
+      case 'scene':
+        return <FileOutlined />;
+      default:
+        return null;
+    }
+  };
+
   return (
-    <div onClick={onClick}>
-      {fileName}
-    </div>
+    <StyledFolderFile onClick={onClick}>
+      {getFileIcon()}
+      <FileName>{fileName}</FileName>
+    </StyledFolderFile>
   );
 };
 
 export default FolderFile;
+
+const StyledFolderFile = styled.div`
+  display: flex;
+  align-items: center;
+  padding-left: 20px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f5f5f5;
+  }
+`;
+
+const FileName = styled.span`
+  margin-left: 10px;
+`;
+
