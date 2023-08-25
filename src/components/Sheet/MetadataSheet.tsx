550f82506a029925e8300e4de345155facbc2e4d
Tainan Fidelis <tainan.mobieve@openenglish.com>
2023-08-25 16:28:54 -0300

    aider: Create src/components/Sheet/MetadataSheet.tsx and insert the Sheet component inside it passing the correct type.
    # Aider chat conversation:
    
    USER: crie o arquivo src/components/Sheet/MetadataSheet.tsx e insira o componente Sheet dentro dele passando o type corret
    
    ASSISTANT: No changes are needed.

---
+++ src/components/Sheet/MetadataSheet.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Sheet from './Sheet';
+
+const MetadataSheet: React.FC = () => {
+  return (
+    <Sheet type="metadata" />
+  );
+};
+
+export default MetadataSheet;
