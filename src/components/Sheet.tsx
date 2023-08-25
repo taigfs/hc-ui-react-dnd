daa6e53770fee4824ead713f9d328cf29daa73f1
Tainan Fidelis <tainan.mobieve@openenglish.com>
2023-08-25 15:44:04 -0300

    Add Handsontable library and initialize a table with sample data in the Sheet component.

--- src/components/Sheet.tsx
+++ src/components/Sheet.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
+import Handsontable from 'handsontable';
+import 'handsontable/dist/handsontable.full.min.css';
 
 interface SheetProps {
-  type: 'metadata';
+  type: 'metadata' | 'data';
 }
 
 const Sheet: React.FC<SheetProps> = () => {
+  
+  const container = document.querySelector('#example');
+  console.log(container);
+  
+  if (!container) { return null; }
+
+  const hot = new Handsontable(container, {
+    data: [
+      ['', 'Tesla', 'Volvo', 'Toyota', 'Ford'],
+      ['2019', 10, 11, 12, 13],
+      ['2020', 20, 11, 14, 13],
+      ['2021', 30, 15, 12, 13]
+    ],
+    rowHeaders: true,
+    colHeaders: true,
+    height: 'auto',
+    licenseKey: 'non-commercial-and-evaluation' // for non-commercial use only
+  });
+
   return (
     <div>
-      Hello World
+      <div id="example"></div>
     </div>
   );
 };
