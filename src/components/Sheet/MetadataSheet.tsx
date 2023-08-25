81880f18cad1bb30af17ba11bd5712ee5f4ce33a
Tainan Fidelis <tainan.mobieve@openenglish.com>
2023-08-25 16:31:26 -0300

    aider: Refactor MetadataSheet component to obtain agentInstances and nodes from the store based on the type, and return a message if currentStory is null.
    # Aider chat conversation:
    
    USER: no arquivo metadatasheet, caso o type seja `agents` obtenha o agentInstances da store. caso currentStory seja null, retorne `Please select an story first.`. caso o type seja `nodes`, obtenha os nodes da store, caso currentStory seja null, retorne `Please select a story first.`. crie o prop s.
    
    ASSISTANT: No changes are needed.

--- src/components/Sheet/MetadataSheet.tsx
+++ src/components/Sheet/MetadataSheet.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Sheet from './Sheet';
+import { RootState } from '../../store';
 
 const MetadataSheet: React.FC = () => {
+  const currentStory = useSelector((state: RootState) => state.story.currentStory);
+  const agentInstances = useSelector((state: RootState) => state.agent.agentInstances);
+  const nodes = useSelector((state: RootState) => state.node.nodes);
+
+  let s = '';
+
+  if (currentStory === null) {
+    if (type === 'agents') {
+      s = 'Please select a story first.';
+    } else if (type === 'nodes') {
+      s = 'Please select a story first.';
+    }
+  }
+
   return (
-    <Sheet type="metadata" />
+    <Sheet type="metadata" s={s} />
   );
 };
 
