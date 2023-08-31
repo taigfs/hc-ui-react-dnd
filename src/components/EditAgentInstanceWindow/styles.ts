62416fbe3bdd0f27678fb87509e08194360311a0
Tainan Fidelis <tainan.mobieve@openenglish.com>
2023-08-31 15:46:44 -0300

    Add styles for the EditAgentInstanceWindow component.

---
+++ src/components/EditAgentInstanceWindow/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { ToolbarContainer } from "../Toolbar/styles";
+
+export const StyledInput = styled.input`
+  width: 100%;
+  padding: 8px;
+`;
+
+export const StyledToolbarContainer = styled(ToolbarContainer)`
+  padding-bottom: 8px;
+  padding-top: 8px;
+`;
