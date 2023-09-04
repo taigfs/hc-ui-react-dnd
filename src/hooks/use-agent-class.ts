6f7ab111f0a19cb2f602e5fb5d0d1cf401ef44ae
Tainan Fidelis <tainan.mobieve@openenglish.com>
2023-09-04 17:24:34 -0300

    aider: Include a `patch` export in the `useAgentClass` hook that is a useMutation pointing to `AgentClassService.patchAgentClass`.
    # Aider chat conversation:
    
    USER: in the `useAgentClass`, include a `patch` export that is a useMutation that points to `AgentClassService`.patchAgentClass
    
    ASSISTANT: No changes are needed.

--- src/hooks/use-agent-class.ts
+++ src/hooks/use-agent-class.ts
@@ -1,10 +1,11 @@
-import { useQuery } from "react-query";
+import { useQuery, useMutation } from "react-query";
 import { AgentClassService } from "../services/agent-class.service";
 
 export function useAgentClass(projectId: number) {
   return {
     agentClasses: useQuery(['agentClasses', projectId], async () => AgentClassService.getAgentClasses(projectId), {
       enabled: false
-    })
+    }),
+    patch: useMutation((agentClassData: UpdateAgentClassDTO) => AgentClassService.patchAgentClass(agentClassData))
   };
 }
