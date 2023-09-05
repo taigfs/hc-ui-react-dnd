df95774c5ddc9b6735234065ab76fcceafe524c1
Tainan Fidelis <tainan.mobieve@openenglish.com>
2023-09-05 19:27:03 -0300

    Add useQueryClient hook to useAgentClass hook.

--- src/hooks/use-agent-class.ts
+++ src/hooks/use-agent-class.ts
@@ -1,8 +1,9 @@
-import { useQuery, useMutation } from "react-query";
+import { useQuery, useMutation, useQueryClient } from "react-query";
 import { AgentClassService } from "../services/agent-class.service";
 import { UpdateAgentClassDTO } from "../dtos/update-agent-class-dto";
 
 export function useAgentClass(projectId: number) {
+  const queryClient = useQueryClient();
   return {
     agentClasses: useQuery(['agentClasses', projectId], async () => AgentClassService.getAgentClasses(projectId), {
       enabled: false
