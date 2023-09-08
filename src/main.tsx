64d9bfd245b755d2b886644d06e037fa1cf72683
Tainan Fidelis <tainan.mobieve@openenglish.com>
2023-09-08 14:25:37 -0300

    Deleted Todo, database, migrations, and schema files, and added LokiJSAdapter and related code in main.tsx.

--- src/main.tsx
+++ src/main.tsx
@@ -23,6 +23,35 @@ import { SpriteLoadProvider } from "./providers/sprite-load-provider";
 import { MetadataPage } from "./pages/MetadataPage";
 import { DataPage } from "./pages/DataPage/DataPage";
 
+import LokiJSAdapter from '@nozbe/watermelondb/adapters/lokijs';
+import { mySchema } from "./models/schema";
+import migrations from "./models/migrations";
+
+const adapter = new LokiJSAdapter({
+  schema: mySchema,
+  migrations,
+  useWebWorker: false,
+  useIncrementalIndexedDB: true,
+  onQuotaExceededError: (error) => {
+    // Browser ran out of disk space -- offer the user to reload the app or log out
+  },
+  onSetUpError: (error) => {
+    // Database failed to load -- offer the user to reload the app or log out
+  },
+  extraIncrementalIDBOptions: {
+    onDidOverwrite: () => {
+      // Called when this adapter is forced to overwrite contents of IndexedDB.
+      // This happens if there's another open tab of the same app that's making changes.
+      // Try to synchronize the app now, and if user is offline, alert them that if they close this
+      // tab, some data may be lost
+    },
+    onversionchange: () => {
+      // database was deleted in another browser tab (user logged out), so we must make sure we delete
+      // it in this tab as well - usually best to just refresh the page
+    },
+  },
+});
+
 const queryClient = new QueryClient();
 
 function App() {
