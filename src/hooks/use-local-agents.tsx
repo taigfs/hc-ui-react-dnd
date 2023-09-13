23f90823857ed7b24d722e8959efe0f4c9cfd25e
Tainan Fidelis <tainan.mobieve@openenglish.com>
2023-09-13 14:55:48 -0300

    Added a new file `use-local-agents.tsx` which contains a custom hook `useLocalAgents` and a context provider `AgentsProvider` for managing local agents.

---
+++ src/hooks/use-local-agents.tsx
@@ -0,0 +1,61 @@
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+import db from '../dexie/database';
+import { AgentInstance } from '../interfaces/AgentInstance';
+
+interface AgentsContextProps {
+  agents: AgentInstance[];
+  get: (id: string) => Promise<AgentInstance | undefined>;
+  getAll: (storyId: string) => void;
+  create: (agent: AgentInstance) => Promise<void>;
+}
+
+const AgentsContext = createContext<AgentsContextProps | undefined>(undefined);
+
+export function AgentsProvider({ children }: { children: ReactNode }) {
+  const [agents, setAgents] = useState<AgentInstance[]>([]);
+
+  // Métodos para buscar um agente por ID, buscar todos os agentes e criar um novo agente
+  const get = async (id: string) => {
+    try {
+      const agent = await db.agents.where('id').equals(id).first();
+      return agent;
+    } catch (error) {
+      console.error('Erro ao buscar agente por ID:', error);
+      throw error;
+    }
+  };
+
+  const getAll = async (storyId: string) => {
+    try {
+      const allAgents = await db.agents.where('storyId').equals(storyId).toArray();
+      setAgents(allAgents);
+    } catch (error) {
+      console.error('Erro ao buscar todos os agentes:', error);
+    }
+  };
+
+  const create = async (agent: AgentInstance) => {
+    try {
+      await db.agents.add(agent);
+      // Atualize a lista de agentes após a criação
+      getAll(agent.storyId);
+    } catch (error) {
+      console.error('Erro ao criar agente:', error);
+      throw error;
+    }
+  };
+
+  return (
+    <AgentsContext.Provider value={{ agents, get, getAll, create }}>
+      {children}
+    </AgentsContext.Provider>
+  );
+}
+
+export function useLocalAgents() {
+  const context = useContext(AgentsContext);
+  if (!context) {
+    throw new Error('useLocalAgents deve ser usado dentro de AgentsProvider');
+  }
+  return context;
+}
