531aa312617d35701ceb694c9ff82a6d06412221
Tainan Fidelis <tainan.mobieve@openenglish.com>
2023-09-25 22:36:14 -0300

    Refactor app providers to include the MapAssetsProvider.

--- src/providers/app-providers.tsx
+++ src/providers/app-providers.tsx
@@ -5,21 +5,24 @@ import { AgentClassesProvider } from "../hooks/use-local-agent-classes";
 import { AgentsProvider } from "../hooks/use-local-agents";
 import { EdgesProvider } from "../hooks/use-local-edges";
 import { ExecutionProvider } from "../hooks/use-local-execution";
+import { MapAssetsProvider } from "../hooks/use-local-map-assets";
 
 export const AppProviders = ({ children }: { children: React.ReactNode }) => (
-  <ScenesProvider>
-    <NodesProvider>
-      <StoriesProvider>
-        <AgentClassesProvider>
-          <AgentsProvider>
-            <EdgesProvider>
-              <ExecutionProvider>
-                {children}
-              </ExecutionProvider>
-            </EdgesProvider>
-          </AgentsProvider>
-        </AgentClassesProvider>
-      </StoriesProvider>
-    </NodesProvider>
-  </ScenesProvider>
+  <MapAssetsProvider>
+    <ScenesProvider>
+      <NodesProvider>
+        <StoriesProvider>
+          <AgentClassesProvider>
+            <AgentsProvider>
+              <EdgesProvider>
+                <ExecutionProvider>
+                  {children}
+                </ExecutionProvider>
+              </EdgesProvider>
+            </AgentsProvider>
+          </AgentClassesProvider>
+        </StoriesProvider>
+      </NodesProvider>
+    </ScenesProvider>
+  </MapAssetsProvider>
 );
