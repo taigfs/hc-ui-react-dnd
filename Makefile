168d6dff22611522af371498f6fdce035d5651f7
Tainan Fidelis <taigfs@gmail.com>
2023-10-03 11:00:42 -0300

    (no commit message provided)

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ up:
 
 open:
 	open -a "Google Chrome" http://localhost:5173/
+
+test-login:
+	curl "http://localhost:3001/open-url?url=https://the-internet.herokuapp.com/login"
--- server/index.js
+++ server/index.js
@@ -9,12 +9,48 @@ app.get('/', (req, res) => {
 });
 
 app.get('/open-url', async (req, res) => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://www.google.com');
-  console.log('URL opened successfully');
-  await browser.close();
-  res.send('URL opened successfully');
+  // curl "http://your-api-server.com/open-url?url=http://example.com"
+
+  const targetUrl = req.query.url;
+
+  // Verificar se a URL é válida
+  if (!targetUrl) {
+    return res.status(400).send('targetUrl is required as a query parameter.');
+  }
+
+  try {
+    
+    const browser = await puppeteer.launch({ headless: false }); // Desativa o modo headless
+
+    const page = await browser.newPage();
+    
+    await page.goto(targetUrl);
+    await page.waitForTimeout(1000);
+
+    console.log('Actions performed successfully');
+    res.send('Actions performed successfully');
+    return;
+
+    // Move o mouse até o input de pesquisa e clica
+    await page.waitForSelector('input[name="q"]');
+    await page.click('input[name="q"]');
+    await page.waitForTimeout(1000);
+
+    // Digita a string no input
+    await page.keyboard.type('o que são agentes autonomos?');
+    await page.waitForTimeout(1000);
+
+    // Move o mouse até o botão de pesquisa e clica
+    await page.waitForSelector('input[name="btnK"]');
+    await page.click('input[name="btnK"]');
+
+    console.log('Actions performed successfully');
+    await browser.close();
+    res.send('Actions performed successfully');
+  } catch (error) {
+    console.error('Error performing actions:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.listen(port, () => {
