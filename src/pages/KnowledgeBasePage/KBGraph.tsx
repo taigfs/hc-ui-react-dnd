d1955370779f623be82f0aa507269f47f1645dc2
Tainan Fidelis <taigfs@gmail.com>
2023-10-04 12:59:00 -0300

    Refactor KBGraph component to use the theme and update SigmaContainer settings.

--- src/pages/KnowledgeBasePage/KBGraph.tsx
+++ src/pages/KnowledgeBasePage/KBGraph.tsx
@@ -2,13 +2,14 @@ import { useEffect } from "react";
 import Graph from "graphology";
 import { SigmaContainer, useLoadGraph } from "@react-sigma/core";
 import "@react-sigma/core/lib/react-sigma.min.css";
+import { useTheme } from "styled-components";
 
 export const LoadGraph = () => {
   const loadGraph = useLoadGraph();
 
   useEffect(() => {
     const graph = new Graph();
-    graph.addNode("first", { x: 0, y: 0, size: 15, label: "My first node", color: "#FFFFFF", labelColor: "#000000" });
+    graph.addNode("first", { x: 0, y: 0, size: 15, label: "My first node" })
     loadGraph(graph);
   }, [loadGraph]);
 
@@ -16,8 +17,19 @@ export const LoadGraph = () => {
 };
 
 export const KBGraph = () => {
+  const theme = useTheme();
   return (
-    <SigmaContainer style={{ height: "500px", width: "500px", backgroundColor: "#000000" }}>
+    <SigmaContainer
+      settings={{
+        defaultNodeColor: theme.color.text,
+        labelColor: {
+          color: theme.color.text,
+        }
+      }}
+      style={{
+        backgroundColor: theme.color.squareBg,
+      }}
+    >
       <LoadGraph />
     </SigmaContainer>
   );
