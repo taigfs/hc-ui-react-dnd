29e528dc60516c4b29d1aba90a5a49e4375a2f1e
Tainan Fidelis <taigfs@gmail.com>
2024-10-29 16:53:24 -0300

    Refactor environment variables and API token handling

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BEARER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRhaWdmc0BnbWFpbC5jb20iLCJzdWIiOiJiOGMwNjhmNy1hMTcyLTRhZTEtYTBiYi03MjZkNzRhMDNhNjMiLCJpYXQiOjE2OTQ1NjMyMTUsImV4cCI6MTcyNjA5OTIxNX0.3unvYhix65DMRqpLcpFpSjofkrVf68UWoEigC5UT8tk';
+const BEARER_TOKEN = import.meta.env.VITE_TOKEN as string;
 
 // Create an axios instance with the Authorization header configured
 const axiosInstance = axios.create({
